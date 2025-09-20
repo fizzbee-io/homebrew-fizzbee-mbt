ed18b4ff536637629aae08dbab24186f93fc17d8
jayaprabhakar <jayaprabhakar@gmail.com>
2025-09-19 19:27:07 -0700

    Fix download URL

--- Formula/fizzbee-mbt.rb
+++ Formula/fizzbee-mbt.rb
@@ -5,10 +5,10 @@ class FizzbeeMbt < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/fizzbee-io/fizzbee-mbt/releases/download/v0.0.1/fizzbee-mbt-0.0.1-macos_arm.tar.gz"
+      url "https://github.com/fizzbee-io/fizzbee-mbt-releases/releases/download/v0.0.1/fizzbee-mbt-0.0.1-macos_arm.tar.gz"
       sha256 "c1509e016010daa691593415579b0b3759be1c031a10524681df59602049d732"
     else
-      url "https://github.com/fizzbee-io/fizzbee-mbt/releases/download/v0.0.1/fizzbee-mbt-0.0.1-macos_x86.tar.gz"
+      url "https://github.com/fizzbee-io/fizzbee-mbt-releases/releases/download/v0.0.1/fizzbee-mbt-0.0.1-macos_x86.tar.gz"
       sha256 "3ee32ca3863bf5625688845d459e1c2e111de78b8fd2076d0354e09f1c601139"
     end
   end
