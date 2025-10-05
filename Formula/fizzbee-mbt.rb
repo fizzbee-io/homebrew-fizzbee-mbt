3bfbd9d9d186f5e6acc78cede7d2b1d286d44b57
fizzbee <152331581+jp-fizzbee@users.noreply.github.com>
2025-10-05 10:40:56 -0700

    Release v0.0.2 (#3)
    Co-authored-by: jayaprabhakar <jayaprabhakar@gmail.com>

--- Formula/fizzbee-mbt.rb
+++ Formula/fizzbee-mbt.rb
@@ -1,15 +1,15 @@
 class FizzbeeMbt < Formula
   desc "Fizzbee MBT - A model-based testing tool"
   homepage "https://github.com/fizzbee-io/fizzbee-mbt"
-  version "0.0.1"
+  version "0.0.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/fizzbee-io/fizzbee-mbt-releases/releases/download/v0.0.1/fizzbee-mbt-0.0.1-macos_arm.tar.gz"
-      sha256 "c1509e016010daa691593415579b0b3759be1c031a10524681df59602049d732"
+      url "https://github.com/fizzbee-io/fizzbee-mbt-releases/releases/download/v0.0.2/fizzbee-mbt-0.0.2-macos_arm.tar.gz"
+      sha256 "d7df8d358012f2b0bef280a83b98608b7dee2cfec7c7293f9b465c9c913d6ed5"
     else
-      url "https://github.com/fizzbee-io/fizzbee-mbt-releases/releases/download/v0.0.1/fizzbee-mbt-0.0.1-macos_x86.tar.gz"
-      sha256 "3ee32ca3863bf5625688845d459e1c2e111de78b8fd2076d0354e09f1c601139"
+      url "https://github.com/fizzbee-io/fizzbee-mbt-releases/releases/download/v0.0.2/fizzbee-mbt-0.0.2-macos_x86.tar.gz"
+      sha256 "1e3138c57414b000737b898a741ba9113c529382f564ff4997921f5d5ab58a45"
     end
   end
 
